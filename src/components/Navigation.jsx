7cde31e6e1b846935b7287581eb32698c58bf4a9
benlazzero <85390423+benlazzero@users.noreply.github.com>
2023-01-28 08:56:43 -0800

    navigation links

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import { container, selected, unselected } from './Navigation.module.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navigation() {
+  const [index, setIndex] = useState(true)
+  
+  const HandleLink = () => {
+      setIndex(!index)
+  }
+
+  return (
+    <div className={container}>
+      <Link to='/' onClick={HandleLink} className={index ? selected : unselected}>
+        BENJAMIN LEE
+      </Link>
+      <Link to='/contact' onClick={HandleLink} className={index ? unselected : selected}>
+        CONTACT
+      </Link>
+    </div>
+  )
+}
