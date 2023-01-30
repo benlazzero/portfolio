8b797ba52f8a8da1dcad3fcacab19ec182f5fb73
benlazzero <85390423+benlazzero@users.noreply.github.com>
2023-01-30 15:59:01 -0800

    plus minus animation refactor

--- src/components/Showcase-Sidebar.jsx
+++ src/components/Showcase-Sidebar.jsx
@@ -1,35 +1,15 @@
-import { useState, useRef } from 'react'
-import { gsap } from 'gsap'
+import { useState } from 'react'
+import Fbar from './ui/Fbar'
 import classes from './Showcase-Sidebar.module.css'
 
 export default function Sidebar() {
   const [isMobile, setMobile] = useState(false)
-  const mobileRef = useRef()
-  const deskRef = useRef()
-  const flatRef = useRef()
-  const flatRef2 = useRef()
   
   const HandleView = () => {
-    const refs = [mobileRef, deskRef, flatRef, flatRef2]
-    
     if (isMobile === true) {
       setMobile(false)
-      refs.forEach((item) => {
-        gsap.to(item.current, {
-          transformOrigin:"50% 50%",
-          rotate: 0,
-        })
-      })
-    }
-    
-    if (isMobile === false) {
+    } else if (isMobile === false) {
       setMobile(true)
-      refs.forEach((item) => {
-        gsap.to(item.current, {
-          transformOrigin:"50% 50%",
-          rotate: item.current.id === "1" ? 180 : 270,
-        })
-      })
     }
   }
   
@@ -39,24 +19,12 @@ export default function Sidebar() {
         <p className={classes.group}>VIEW</p>
         <div className={classes.itemWrapper}>
           <div onClick={HandleView} className={`${classes.icon} ${isMobile ? classes.selected : ''}`}>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
-              <line ref={flatRef2} id="1" x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-              <line ref={mobileRef} x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-
+            <Fbar plus={isMobile ? true : false}/>
             <p className={classes.item}>Mobile</p>
-
           </div>
           <div onClick={HandleView} className={`${classes.icon} ${isMobile ? '' : classes.selected}`}>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
-              <line ref={flatRef} id="1" x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-              <line ref={deskRef} x1="5" y1="1" x2="5" y2="9" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-
+            <Fbar plus={isMobile ? false : true}/>
             <p className={classes.item}>Desk</p>
-
           </div>
         </div>
       </div>
--- src/components/ui/Fbar.jsx
+++ src/components/ui/Fbar.jsx
@@ -1,8 +1,28 @@
-export default function Fbar() {
+import { forwardRef, useRef, useEffect } from "react"
+import { gsap } from 'gsap'
+
+const Fbar = forwardRef((props, ref) => {
+  const changeRef = useRef()
+  const spinRef = useRef()
+  const isPlus = props.plus
+
+  useEffect(() => {
+    gsap.to(changeRef.current, {
+      transformOrigin:"50% 50%",
+      rotate: isPlus ? 270 : 0,
+    })
+    gsap.to(spinRef.current, {
+      transformOrigin:"50% 50%",
+      rotate: isPlus ? 180 : 0,
+    })
+  }, [isPlus])
+
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
-      <line id="1" x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-      <line x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
+    <svg ref={ref} xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+      <line ref={changeRef} id="1" x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
+      <line ref={spinRef} x1="1" y1="5" x2="9" y2="5" stroke="black" strokeWidth="2" strokeLinecap="round"/>
     </svg>
   )
-}
+})
+
+export default Fbar
