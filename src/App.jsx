5212deee0728dffb05fb9be3fdc251ea026e6d02
benlazzero <85390423+benlazzero@users.noreply.github.com>
2023-02-04 15:20:37 -0800

    rats animating

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Suspense, useRef } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import Navigation from './components/Navigation';
 import Showcase from './components/Showcase';
 import { container } from './App.module.css'
 
-
 function Rat(props) {
   const ref = useRef()
   const { viewport } = useThree()
   const { nodes, materials } = useGLTF('/ratr.glb')
-  
-  console.log(ref.current)
+  const [ratFlag, setRatFlag] = useState(false)
   
   useFrame((state) => {
-    ref.current.rotation.z = Math.sin(state.clock.elapsedTime * 10) * .01
-    ref.current.position.y += .01
-    if (ref.current.position.y > viewport.height / 1) {
-      ref.current.position.y = -viewport.height / 2
+    ref.current.rotation.z = Math.sin(state.clock.elapsedTime * 10) * .004
+    if (ratFlag) {
+      ref.current.position.x += .02
+      if (ref.current.position.x > viewport.width / .8) {
+        setRatFlag(!ratFlag)
+      }
+    } else {
+      ref.current.position.x -= .02
+      if (ref.current.position.x < -viewport.width / .8) {
+        setRatFlag(!ratFlag)
+      }
     }
   })
   
   return (
-  <group {...props} dispose={null} ref={ref}>
-    <mesh 
-      geometry={nodes.Main_0.geometry}
-      material={materials.Rat_Main_Material}
-      material-emissive='black'
-      scale={.04}
-      position={[.3, -3.3, 0]}
-    />
-    <mesh 
-      geometry={nodes.Main_0.geometry}
-      material={materials.Rat_Main_Material}
-      material-emissive='black'
-      scale={.04}
-      position={[0, -3, 0]}
-    />
-  </group>
+    <group {...props} dispose={null} ref={ref}>
+      <mesh 
+        geometry={nodes.Main_0.geometry}
+        material={materials.Rat_Main_Material}
+        material-emissive='black'
+        scale={.04}
+        rotation={[ratFlag ? .5 : -.5, 0, ratFlag ? 11 : 1.6]}
+        position={[ratFlag ? -6.6 : 6.6, ratFlag ? viewport.height / 2.2 : -viewport.height / 2.3 , 0]}
+      />
+      <mesh 
+        geometry={nodes.Main_0.geometry}
+        material={materials.Rat_Main_Material}
+        material-emissive='black'
+        scale={.04}
+        rotation={[ratFlag ? .5 : -.5, 0, ratFlag ? 11 : 1.6]}
+        position={[ratFlag ? -6.2 : 6.2, ratFlag ? viewport.height / 2.1 : -viewport.height / 2.5 , 0]}
+      />
+    </group>
   )
-
 }
 
 export default function App() {
